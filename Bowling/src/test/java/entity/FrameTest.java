b9823b3bc741e844ddede8344efa33751759303c
loic <loic.gstrn@gmail.com>
2024-01-31 12:06:13 +0100

    test

--- Bowling/src/test/java/entity/FrameTest.java
+++ Bowling/src/test/java/entity/FrameTest.java
@@ -1,4 +1,10 @@
 package entity;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class FrameTest {
+
+
 }
